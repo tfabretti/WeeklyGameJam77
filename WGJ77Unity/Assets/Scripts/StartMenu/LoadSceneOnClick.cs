fab20e3d29be2a84b5066824833566d9710af3d8
tfabretti <thomas.fabretti@hotmail.fr>
2019-01-02 10:45:30 +0100

    Main menu addition
    New scene + transition

---
+++ WGJ77Unity/Assets/Scripts/StartMenu/LoadSceneOnClick.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadSceneOnClick : MonoBehaviour
+{
+    public void LoadByIndex( int p_sceneIndex )
+    {
+        SceneManager.LoadScene( p_sceneIndex);
+    }
+
+    public void LoadByName( string p_strSceneName )
+    {
+        SceneManager.LoadScene( p_strSceneName );
+    }
+}
