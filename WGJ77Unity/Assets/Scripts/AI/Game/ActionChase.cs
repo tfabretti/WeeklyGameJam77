5c409e1f8dd369cfb837f89feca4136a2784bc0e
tfabretti <thomas.fabretti@hotmail.fr>
2019-01-03 13:39:01 +0100

    AI update

---
+++ WGJ77Unity/Assets/Scripts/AI/Game/ActionChase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu( menuName = "PluggableAI/Actions/Chase" )]
+public class ActionChase : AI_Action
+{
+    public override void Act( AI_StateController p_controller )
+    {
+        Chase( ( StateController)p_controller );
+    }
+
+    private void Chase( StateController p_controller )
+    {
+        p_controller.navMeshAgent.destination = p_controller.m_chaseTarget.position;
+        p_controller.navMeshAgent.isStopped = false;
+    }
+
+}
--- WGJ77Unity/Assets/Scripts/AI/Game/StateController.cs
+++ WGJ77Unity/Assets/Scripts/AI/Game/StateController.cs
@@ -7,28 +7,24 @@ public class StateController : AI_StateController
 {
 	public EnemyStats m_gameEnemyStats;
 
-	[HideInInspector] public NavMeshAgent navMeshAgent;
+    public Transform m_chaseTarget;
+
+    [HideInInspector] public NavMeshAgent navMeshAgent;
 	[HideInInspector] public List<Transform> wayPointList;
     [HideInInspector] public int nextWayPoint;
-
-    private bool aiActive;
 
 	void Awake () 
 	{
         m_enemyStats = m_gameEnemyStats;
 		navMeshAgent = GetComponent<NavMeshAgent> ();
-	}
+        m_isAIActive = true;
 
-	public void SetupAI(bool aiActivationFromTankManager, List<Transform> wayPointsFromTankManager)
+    }
+
+	public void SetupAI(bool p_IsAIActive)
 	{
-		wayPointList = wayPointsFromTankManager;
-		aiActive = aiActivationFromTankManager;
-		if (aiActive) 
-		{
-			navMeshAgent.enabled = true;
-		} else 
-		{
-			navMeshAgent.enabled = false;
-		}
+		m_isAIActive = p_IsAIActive;
+        if ( navMeshAgent )
+            navMeshAgent.enabled = m_isAIActive;
 	}
 }
